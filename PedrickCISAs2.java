498d5594b395b6f5eab2e26c43b9d9a08c39c066
Ethan <ethan.pedrick@gmail.com>
2023-09-12 21:03:37 -0400

    c

---
+++ PedrickCISAs2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class PedrickCISAs2 {
+    public static void main(String[] args) {
+
+    }
+//process the input and output
+//create and search the array
+
+//create an array of BankAccount
+}
+
+class BankAccount{
+//Used to handle objects (bank accounts)
+}
+
+/*Ask the user to input the number of bank accounts that will be created
+The number should be 1-10
+Otherwise it should say it can't create that many accounts and go again
+Each element of the array will refer to a bank account
+Prompt the user to input details of each bank account
+Details: owner, number, and balance
+Use the BankAccount constructor to create a BankAccount object
+Assign the object to an array element and have the details
+
+
+
+*/
