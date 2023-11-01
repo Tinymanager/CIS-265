e11283a78f68fb3c5a37fe41c56cbaa7ce0532c9
Ethan Pedrick <ethan.pedrick@gmail.com>
2023-11-01 14:42:29 -0400

    c

--- PedrickAssignment8.java
+++ PedrickAssignment8.java
@@ -79,27 +79,8 @@ public class PedrickAssignment8 {
 
 
 /*
- * Create a LinkedList and ArrayList of ints with 100,000 random ints
- * Measure and print the time to:
- * get 1,000 numbers at random positions from both
- * insert 1,000 numbers in the beginning of both
- * remove 1,000 numbers at random positions from both
- * 
- * Print the time in milliseconds (1/1000 seconds)
- * 
- * Random rand = new Random();
- * randomInt = rand.nextInt(0, MAXINT);
- * System.nanoTime() to get the timer
- * nano = 1/1000000000 seconds
- * 
- * long startTime = System.nanoTime();
- * //Code
- * long endTime = System.nanoTime();
- * long executionTime = (endTime - startTime);
- * //convert to milliseconds
- * 
- * Print:
- * "Time for get() in List: "
- * "Time for insertion in List: "
- * "Time for deletion in List: "
+ * Name: Ethan Pedrick
+ * CSU ID: 2835438
+ * CIS 265: Assignment 8
+ * Description: Comparing the performance of ArrayList and LinkedList
  */
