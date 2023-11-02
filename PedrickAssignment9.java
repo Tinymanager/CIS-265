8118d41a26052f485049543eda910c2543934ab5
Ethan Pedrick <ethan.pedrick@gmail.com>
2023-11-02 14:45:32 -0400

    c

---
+++ PedrickAssignment9.java
@@ -0,0 +1,25 @@
+public class PedrickAssignment9 {
+    public static void main(String[] args){
+
+        //BubbleSort
+
+
+        //InsertionSort
+
+
+        //MergeSort
+
+
+        //QuickSort
+
+
+        //Collections.sort()
+    }
+}
+
+/*
+ * Declare an array of 50,000 integers
+ * Fill it with random ints
+ * Calculate the time to sort it
+ * Print the time in ms
+ */
